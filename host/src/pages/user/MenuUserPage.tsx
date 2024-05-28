64cd9b6fecabb004224c7af448d2c53f2cf096fa
julianrodriguez01 <julian.rodriguez21@uptc.edu.co>
2024-05-27 21:09:18 -0500

    Fix menu user

--- host/src/pages/user/MenuUserPage.tsx
+++ host/src/pages/user/MenuUserPage.tsx
@@ -3,39 +3,42 @@ import React from 'react'
 import SideBar from 'side_bar/SideBar'
 import CreateTurn from 'create_turn/CreateTurn'
 
-const props = {
-  options: [
-    {
-      title: 'Ver Turnos',
-      icon: 'ic:baseline-format-list-bulleted',
-      onClick: () => {
-        console.log('Home')
-      },
-    },
-    {
-      title: 'Crear Turno',
-      icon: 'ic:outline-add-box',
-      onClick: () => {
-        console.log('Home')
-      },
-    },
-  ],
-  user: {
-    name: 'Jack Herrington',
-    role: 'Admin',
+const MenuAdminPage = () => {
+  const [user, setUser] = useState({
+    name: 'None',
+    role: 'User',
     onClickLogout: () => {
-      console.log('Logout')
+      console.log('Logout');
+      localStorage.removeItem('user');
     },
-  },
-}
+  });
 
-function CreateTurnPage() {
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  const props = {
+    options: [
+      {
+        title: 'Home',
+        icon: 'material-symbols:home-outline-rounded',
+        onClick: () => {
+          console.log('Home');
+        },
+      },
+    ],
+    user: user,
+  };
+
   return (
     <div>
       <SideBar options={props.options} user={props.user} />
-      <CreateTurn/>
+      <CreateTurn />
     </div>
-  )
-}
+  );
+};
 
-export default CreateTurnPage;
+export default MenuUserPage;
